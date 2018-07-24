e7c7fd9706d2882f3ed84bc782dba107c07ea29d
Hackcraft <zakary@zakary.org>
2018-07-24 16:48:52 +0100

    Changed base_gmodentity to base_entity to reduce the size of the meta table inheritance as it doesn't seem like it's needed.
    Changed the render group from RENDERGROUP_BOTH to RENDERGROUP_TRANSLUCENT and changed ENT:Draw to ENT:DrawTranslucent as ENT:Draw allowed materials to be shown in front of the text. For example when looking at windows or a prop with a custom material applied.

--- lua/entities/sammyservers_textscreen/shared.lua
+++ lua/entities/sammyservers_textscreen/shared.lua
@@ -1,10 +1,10 @@
 ENT.Type = "anim"
-ENT.Base = "base_gmodentity"
+ENT.Base = "base_entity"
 ENT.PrintName = "SammyServers Textscreen"
 ENT.Author = "SammyServers"
 ENT.Spawnable = false
 ENT.AdminSpawnable = false
-ENT.RenderGroup = RENDERGROUP_BOTH
+ENT.RenderGroup = RENDERGROUP_TRANSLUCENT
 
 function ENT:SetupDataTables()
 	self:NetworkVar("Bool", 0, "IsPersisted")
