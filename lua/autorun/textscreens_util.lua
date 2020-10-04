e911517b30925452e21901f6f82f765d16f4f821
zak <19314863+zakarybk@users.noreply.github.com>
2020-10-04 21:59:16 +0100

    Added textscreen entity to pocket blacklist for DarkRP and gamemodes based off of DarkRP

--- lua/autorun/textscreens_util.lua
+++ lua/autorun/textscreens_util.lua
@@ -163,6 +163,12 @@ if SERVER then
 			return printMessage(ply, "Textscreen removed and is no longer permanent.")
 		end
 	end)
+
+	-- Add to pocket blacklist for DarkRP
+	-- Not using gamemode == "darkrp" because there are lots of flavours of darkrp
+	hook.Add("loadCustomDarkRPItems", "sammyservers_pocket_blacklist", function()
+		GAMEMODE.Config.PocketBlacklist["sammyservers_textscreen"] = true
+	end)
 end
 
 if CLIENT then
